d5a47163cee718bef95fc5bd189c4d55e5d01c8b
Hajra <72098999+hajraladha@users.noreply.github.com>
2022-01-07 18:08:03 +0000

    jQuery code

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+//Adding new item to List
+
+function newItem() {
+    let list = $('#list');
+    let li = $('<li></li>');
+    let input = $('#input');
+    let inputValue = $('#input').val();
+
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('Cannot be blank!');
+    } else {
+        $('#list').append(li);
+        $('#input').val('');
+    }
+
+
+    //Crossing out item from List
+
+    function crossOut() {
+        li.toggleClass('strike');
+        let done = $('#done');
+        done.append(li);
+    }
+
+    li.on('dblclick', crossOut);
+
+    //Add delete button
+    let crossOutButton = $('<crossOutButton></crossOutButton>')
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+    crossOutButton.on('click', deleteListItem);
+
+    //Make list sortable
+
+    $('#list').sortable()
+}
+
+
+
+
+
+
+
